8689c5b64bbcec6758112e3fe3a00469f60a3f6d
valery-designer <valery-designer@yandex.ru>
2023-07-07 12:35:02 +0300

    Demo update

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,96 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import ImageList from '@mui/material/ImageList';
+import ImageListItem from '@mui/material/ImageListItem';
+import { useRef } from 'react';
+import Lightbox from 'finally-lightbox-react';
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const lightboxReference = useRef({});
+  const pets = [ 
+    { src:'dog01.jpg', title: 'My dog' }, 
+    { src:'cat01.jpg', title: 'My cat' }, 
+    { src:'dog02.jpg', title: 'Another dog' } 
+  ];
+  const cities = [ 
+    { src:'city01.jpg' }, 
+    { src:'city02.jpg' }, 
+    { src:'city03.jpg' },
+    { src:'city04.jpg' },
+  ];
+  const people = [ 
+    { src:'people01.jpg' },
+    { src:'people02.jpg' },
+    { src:'people03.jpg' },
+    { src:'people04.jpg' } 
+  ];
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="App">
+
+        <h1>Single image</h1>
+        <img className="single_image"
+          src='dog01.jpg'
+          alt='My dog'
+          onClick = { (e)=>{ lightboxReference.current.openInLightbox(e, [{src:'dog01.jpg'}]); } }
+          //loading="lazy"
+        />
+
+        <h3><br/><br/>Gallery 1</h3>
+        <h1>Pets</h1>
+        <ImageList cols={3} variant={'quilted'}>
+          { pets.map((item) => (
+            <ImageListItem sx={{ aspectRatio: '1 / 1' }} key={item.src}>
+              <img
+                src={item.src}
+                alt={item.title}
+                onClick = { (e)=>{ lightboxReference.current.openInLightbox(e, pets); } }
+                //loading="lazy"
+              />
+            </ImageListItem>
+          ))}
+        </ImageList>
+
+        <h3>Gallery 2</h3>
+        <h1>Cities</h1>
+        <ImageList cols={2} variant={'quilted'}>
+          { cities.map((item) => (
+            <ImageListItem sx={{ aspectRatio: '1 / 1' }} key={item.src}>
+              <img
+                src={`${item.src}?w=200&h=200&fit=crop&auto=format`}
+                srcSet={`${item.src}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
+                alt={item.title}
+                // onClick = { (e)=>{ Lightbox.instance.openInLightbox(e, cities); } }
+                onClick = { (e)=>{ lightboxReference.current.openInLightbox(e, cities); } }
+                //loading="lazy"
+              />
+            </ImageListItem>
+          )) }
+        </ImageList>
+
+        <h3>Gallery 3</h3>
+        <h1>People</h1>
+        <ImageList cols={2} variant={'quilted'}>
+          { people.map((item) => (
+            <ImageListItem sx={{ aspectRatio: '1 / 1' }} key={item.src}>
+              <img
+                src={item.src}
+                alt={item.title}
+                // onClick = { (e)=>{ Lightbox.instance.openInLightbox(e, cities); } }
+                onClick = { (e)=>{ lightboxReference.current.openInLightbox(e, people); } }
+                //loading="lazy"
+              />
+            </ImageListItem>
+          )) }
+        </ImageList>
+
+        {/* <div className="gallery_link" onClick = { (e)=>{ lightboxReference.current.openInLightbox(e, people); } }>Link</div> */}
+
+
+        <Lightbox lightboxReference={lightboxReference} />
+
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
 
-export default App
+export default App;
